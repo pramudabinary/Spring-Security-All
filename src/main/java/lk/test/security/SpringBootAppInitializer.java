0d58a7dc25c5617718623c0debc0a7b04613f947
Pramuda Liyanage <pramudatharika@gmail.com>
2021-12-05 05:38:25 +0530

    first commit

---
+++ src/main/java/lk/test/security/SpringBootAppInitializer.java
@@ -0,0 +1,18 @@
+package lk.test.security;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pramuda Liyanage <pramudatharika@gmail.com>
+ * @since 12/5/21
+ **/
+
+@SpringBootApplication
+public class SpringBootAppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppInitializer.class);
+    }
+
+}
